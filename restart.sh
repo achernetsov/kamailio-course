987fe9f6a2790d1af3fd9df934fa439d9a241a61
Artyom Chernetsov <chernetsov.artyom@gmail.com>
2025-04-13 19:24:48 +0600

    Registrar: internal b2bua auth

--- restart.sh
+++ restart.sh
@@ -11,3 +11,12 @@ if [ $1 = 'e1' ]; then
     exit 0
 fi
 
+if [ $1 = 'i1' ]; then
+    echo 'docker restart b2bua_internal_01'
+    docker stop b2bua_internal_01
+    docker rm b2bua_internal_01
+    docker-compose up b2bua_internal_01 -d
+    exit 0
+fi
+
+
