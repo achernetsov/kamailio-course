567c04319f00cee2cb471eabf1ccac7656687096
Artyom Chernetsov <chernetsov.artyom@gmail.com>
2025-04-13 19:03:30 +0600

    Registrar: db auth

--- restart.sh
+++ restart.sh
@@ -1,1 +1,13 @@
-docker restart kamailio-edge
+if [ $# -eq 0 ]
+  then
+    echo "docker restart kamailio-edge"
+    docker restart kamailio-edge
+    exit 0
+fi
+
+if [ $1 = 'e1' ]; then
+    echo 'docker restart b2bua_external_01'
+    docker restart b2bua_external_01
+    exit 0
+fi
+
